d380e49c2bb05fa9bdaa2d8a98b11ec6c7da6bfa
Suchmxn Dev Services <122735563+SUCHMXN@users.noreply.github.com>
2026-01-07 23:37:35 -0500

    Update register-commands.js

--- register-commands.js
+++ register-commands.js
@@ -36,7 +36,6 @@ const rest = new REST({ version: '10' }).setToken(TOKEN);
   try {
     console.log('Started refreshing application (/) commands.');
     await rest.put(Routes.applicationGuildCommands(CLIENT_ID, GUILD_ID), { body: commands });
-    // For global: await rest.put(Routes.applicationCommands(CLIENT_ID), { body: commands });
     console.log('Successfully reloaded application (/) commands.');
   } catch (error) {
     console.error(error);
